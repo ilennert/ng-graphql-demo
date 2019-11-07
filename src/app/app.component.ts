0f02ab2c784c89649bf4db40e5186a33d51f93ae
Irv Lennert <irv.lennert@gmail.com>
2019-11-06 21:13:32 -0500

    Fix ng-bootstrap issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'The Pet Sanctuary Center';
+
+  constructor() { }
 }
--- src/app/sanctuary/containers/sanctuary-center/sanctuary-center.component.ts
+++ src/app/sanctuary/containers/sanctuary-center/sanctuary-center.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// import { ToastService } from '../../../services/toast.service';
 
 @Component({
   selector: 'app-sanctuary-center',
   templateUrl: './sanctuary-center.component.html',
   styleUrls: ['./sanctuary-center.component.scss']
 })
-export class SanctuaryCenterComponent implements OnInit {
+export class SanctuaryCenterComponent {
 
-  constructor() { }
+  constructor(/* public toastService: ToastService */) { }
 
-  ngOnInit() {
-  }
+  // showStandard() {
+  //   this.toastService.show('I am a standard toast');
+  // }
 
+  // showSuccess() {
+  //   this.toastService.show('I am a success toast', { classname: 'bg-success text-light', delay: 10000 });
+  // }
+
+  // showDanger(dangerTpl) {
+  //   this.toastService.show(dangerTpl, { classname: 'bg-danger text-light', delay: 15000 });
+  // }
 }
