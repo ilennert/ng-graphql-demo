3f02e5ec034bfe7d01c181de61be68253d309032
Irv Lennert <irv.lennert@gmail.com>
2019-12-06 02:39:27 -0500

    Alert formatting, cleanup

--- src/app/sanctuary/containers/pet-detail/pet-detail.component.ts
+++ src/app/sanctuary/containers/pet-detail/pet-detail.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
-
+import { map } from 'rxjs/operators';
 import { State } from '../../../store';
 import { Pet } from '../../model/pet';
-import { Range } from '../../model/range';
 import * as fromSelectors from '../../store/selectors';
-import * as fromRoot from '../../../store';
 
 @Component({
   selector: 'app-pet-detail',
@@ -14,6 +12,20 @@ import * as fromRoot from '../../../store';
   styleUrls: ['./pet-detail.component.scss']
 })
 export class PetDetailComponent {
+  private month = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
   alertType = [
     'success',
     'info',
@@ -23,20 +35,43 @@ export class PetDetailComponent {
     'secondary'
   ];
   pet$: Observable<Pet>;
-  historyByIdD$: Observable<(id: string) => Range>;
 
   constructor(private store: Store<State>) {
     this.pet$ = this.store.pipe(
       select(fromSelectors.selectCurrentPet)
     );
-    this.historyByIdD$ = this.store.pipe(
-      select(fromSelectors.selectHistoryByIdD)
+  }
+
+  getHistoryItem(rangeId: string, pet: Pet): Observable<string> {
+    return this.store.pipe(
+      select(fromSelectors.selectHistoryByIdD),
+      map(f => {
+        const range = f(rangeId);
+        const formatDate = this.formatDateTime(range.transactionDate);
+        let retVal: string;
+        if (!range.ownerId && range.sanctuaryId) {
+          retVal = `${pet.name} a ${pet.species}, on ${formatDate}, was brought into the Sanctuary`;
+          retVal += ' as a stray, looking for a new home.';
+          return retVal;
+        } else if (range.ownerId && range.sanctuaryId && !range.toOwner) {
+          retVal = `${pet.name} a ${pet.species}, on ${formatDate}, was brought into the Sanctuary`;
+          retVal += ' as an orphan, looking for a new home.';
+          return retVal;
+        } else {
+          return `${pet.name} a ${pet.species}, has found a new home on ${formatDate}.`;
+        }
+      })
     );
   }
 
-  getHistoryItem(range: Range, pet: Pet): string {
-    console.log(range.petId);
-    return range.petId;
+  private formatDateTime(ind: Date): string {
+    const d = new Date(ind);
+    const retVal = (this.month[d.getMonth()])
+      + ' ' + d.getDate()
+      + ', ' +  d.getFullYear()
+      + ' at '
+      + d.getHours()
+      + ':' + d.getMinutes();
+    return retVal;
   }
-
 }
